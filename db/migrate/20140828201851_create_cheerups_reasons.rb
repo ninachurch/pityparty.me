3b3279ba9e6e24df0ca44d86b22a71c7a3324a31
Nina Church <ninapearlchurch@gmail.com>
2014-08-28 15:40:06 -0700

    deleted bad migration

--- db/migrate/20140828201851_create_cheerups_reasons.rb
+++
@@ -1,8 +0,0 @@
-class CreateCheerupsReasons < ActiveRecord::Migration
-  def change
-    create_table :cheerups_reasons do |t|
-      t.references :reason
-      t.references :cheerup
-    end
-  end
-end

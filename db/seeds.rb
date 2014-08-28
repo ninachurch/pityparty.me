3237e7126fce4d08156fe099e3c20296bf7ed5f3
Nina Church <ninapearlchurch@gmail.com>
2014-08-28 15:40:56 -0700

    functional seeder file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,36 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'faker'
+
 
 ##image links
 
 @images = ["http://i.imgur.com/EaNmbVG.jpg?1", "http://i.imgur.com/HGzWgjj.jpg?4883", "http://i.imgur.com/pdEal7q.jpg", "http://i.imgur.com/LtDyYKS.jpg", "http://i.imgur.com/umSuskL.jpg", "http://i.imgur.com/EZZmWHB.jpg", "http://i.imgur.com/NHYBHpL.jpg", "http://i.imgur.com/hMzWUTv.jpg", "http://i.imgur.com/jScJ09K.jpg", "http://i.imgur.com/pcw2EKe.jpg", "http://i.imgur.com/ODypwpx.jpg", "http://i.imgur.com/1Fb0U02.jpg", "http://i.imgur.com/Oeig4I4.jpg", "http://i.imgur.com/kgCQ0uD.jpg"]
 
+@reason_names = ["Family is Crazy", "Significant Other Suckiness", "No Friends", "I'm a Princess", "Fucking School", "No Money No Fun", "Just Pity Me", "I'm Fat"]
+
 ##create fake reasons
-10.times do
-  Reason.create(name: Faker::Commerce.product_name)
+@reason_names.each do |reason|
+  Reason.create(name: reason)
 end
 
-15.times do
-  Cheerup.create(visual_source: @images[cycle])
+##create fake cheerups
+40.times do
+  Cheerup.create(visual_source: @images.sample)
 end
 
-@themes = Theme.all
-##create sample craft objects
-14.times do |cycle|
-  Craft.create(name: Faker::Commerce.product_name,
-              description: Faker::Company.catch_phrase,
-              source_url: Faker::Internet.url('example.com'),
-              image_url: @images[cycle],
-              theme_id: @themes.sample.id)
+@reasons = Reason.all
+@cheerups = Cheerup.all
+
+@reasons.each do |reason|
+  10.times do
+    reason.cheerups << @cheerups.sample
+  end
 end
 
-##create 5 items for each craft
-crafts =  Craft.all
-crafts.each_with_index do |craft, index|
-  @craft_id = index + 1
-  5.times do
-    Item.create(craft_id: @craft_id, name: @search_items.sample)
+@cheerups.each do |cheerup|
+  3.times do
+    cheerup.reasons << @reasons.sample
   end
 end

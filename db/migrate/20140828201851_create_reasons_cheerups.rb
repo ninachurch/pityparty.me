fe27a761b35178ea1df1fb960e5d2b037c7aff9f
Nina Church <ninapearlchurch@gmail.com>
2014-08-28 14:31:41 -0700

    changed alphatbetized

--- db/migrate/20140828201851_create_reasons_cheerups.rb
+++
@@ -1,8 +0,0 @@
-class CreateReasonsCheerups < ActiveRecord::Migration
-  def change
-    create_table :reasons_cheerups do |t|
-      t.integer :reason_id
-      t.integer :cheerup_id
-    end
-  end
-end
